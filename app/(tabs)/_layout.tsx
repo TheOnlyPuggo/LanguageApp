66b103a42efe812ea210ab2bebf2fc9f180d04c2
PuttPuttTheTruck <saulmiroullman@gmail.com>
2025-05-02 13:42:33 +1000

    Added Icons To Bottom

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,11 +1,42 @@
 import { Tabs } from "expo-router"
+import { Image } from 'react-native';
 
 export default function RootLayout() {
     return (
         <Tabs>
-            <Tabs.Screen name="index" options={{title: "Learning", headerShown: false}} />
-            <Tabs.Screen name="leaderboard" options={{title: "Leaderboard", headerShown: false}} />
-            <Tabs.Screen name="profile" options={{title: "Profile", headerShown: false}} />
+            <Tabs.Screen name="index" options={{title: "Learning", headerShown: false, tabBarIcon: ({ color, focused }) => (
+                        <Image
+                            source={require('../../assets/LearnPageIcon.png')}
+                            style={{
+                                width: 24,
+                                height: 24,
+                                tintColor: color,
+                                opacity: focused ? 1 : 0.6,
+                            }}
+                        />
+                    ),}} />
+            <Tabs.Screen name="leaderboard" options={{title: "Leaderboard", headerShown: false, tabBarIcon: ({ color, focused }) => (
+                        <Image
+                            source={require('../../assets/Leaderboards.png')}
+                            style={{
+                                width: 24,
+                                height: 24,
+                                tintColor: color,
+                                opacity: focused ? 1 : 0.6,
+                            }}
+                        />
+                    ),}} />
+            <Tabs.Screen name="profile" options={{title: "Profile", headerShown: false, tabBarIcon: ({ color, focused }) => (
+                        <Image
+                            source={require('../../assets/ProfilePageIcon.png')}
+                            style={{
+                                width: 24,
+                                height: 24,
+                                tintColor: color,
+                                opacity: focused ? 1 : 0.6,
+                            }}
+                        />
+                    ),}} />
         </Tabs>
     )
 }
