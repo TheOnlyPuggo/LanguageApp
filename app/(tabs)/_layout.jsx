4f87924122267a9618a6b78f1192f388841f7377
Pug <iakingdoms@gmail.com>
2025-05-05 14:43:58 +1000

    Updated navbar

--- app/(tabs)/_layout.jsx
+++ app/(tabs)/_layout.jsx
@@ -1,42 +1,45 @@
 import { Tabs } from "expo-router"
-import { Image } from 'react-native';
+import { Image, View } from 'react-native';
 
 export default function RootLayout() {
     return (
         <Tabs>
-            <Tabs.Screen name="index" options={{title: "Learning", headerShown: false, tabBarIcon: ({ color, focused }) => (
-                <Image
-                    source={require('../../assets/LearnPageIcon.png')}
-                    style={{
-                        width: 24,
-                        height: 24,
-                        tintColor: color,
-                        opacity: focused ? 1 : 0.6,
-                    }}
-                />
-            ),}} />
-            <Tabs.Screen name="leaderboard" options={{title: "Leaderboard", headerShown: false, tabBarIcon: ({ color, focused }) => (
-                <Image
-                    source={require('../../assets/Leaderboards.png')}
-                    style={{
-                        width: 24,
-                        height: 24,
-                        tintColor: color,
-                        opacity: focused ? 1 : 0.6,
-                    }}
-                />
-            ),}} />
-            <Tabs.Screen name="profile" options={{title: "Profile", headerShown: false, tabBarIcon: ({ color, focused }) => (
-                <Image
-                    source={require('../../assets/ProfilePageIcon.png')}
-                    style={{
-                        width: 24,
-                        height: 24,
-                        tintColor: color,
-                        opacity: focused ? 1 : 0.6,
-                    }}
-                />
-            ),}} />
+            <Tabs.Screen name="index" options={{title: "Learning", headerShown: false, tabBarLabel: () => null, tabBarIcon: ({ color, focused }) => (
+                <View style={{ paddingTop: "10" }}>
+                    <Image
+                        source={require('../../assets/LearnPageIcon.png')}
+                        style={{
+                            width: 24,
+                            height: 24,
+                            opacity: focused ? 1 : 0.5,
+                        }}
+                    />
+                </View>
+            )}} />
+            <Tabs.Screen name="leaderboard" options={{title: "Leaderboard", headerShown: false, tabBarLabel: () => null, tabBarIcon: ({ color, focused }) => (
+                <View style={{ paddingTop: "10" }}>
+                    <Image
+                        source={require('../../assets/Leaderboards.png')}
+                        style={{
+                            width: 30,
+                            height: 24,
+                            opacity: focused ? 1 : 0.5,
+                        }}
+                    />
+                </View>
+            )}} />
+            <Tabs.Screen name="profile" options={{title: "Profile", headerShown: false, tabBarLabel: () => null, tabBarIcon: ({ color, focused }) => (
+                <View style={{ paddingTop: "10" }}>
+                    <Image
+                        source={require('../../assets/ProfilePageIcon.png')}
+                        style={{
+                            width: 24,
+                            height: 24,
+                            opacity: focused ? 1 : 0.5,
+                        }}
+                    />
+                </View>
+            )}} />
         </Tabs>
     )
 }
